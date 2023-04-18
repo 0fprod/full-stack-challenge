99c635a891ba632f7e4d1d14278132d45fe43ba0
franjpr <franp.dev@protonmail.com>
2023-04-18 17:46:16 +0100

    backend: fix: monster controller test

--- backend/src/modules/monster/monster.controller.spec.ts
+++ backend/src/modules/monster/monster.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MonsterController } from './monster.controller';
+import { MonsterService } from './monster.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Monster } from '../../database/schemas/monster.schema';
 
 describe('MonsterController', () => {
   let controller: MonsterController;
@@ -7,6 +10,17 @@ describe('MonsterController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MonsterController],
+      providers: [
+        MonsterService,
+        {
+          provide: getModelToken(Monster.name),
+          useValue: {
+            find: jest.fn(),
+            create: jest.fn(),
+            exec: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<MonsterController>(MonsterController);
