f436905f95fda52eba96ea0105033c26c9feb8b3
franjpr <franp.dev@protonmail.com>
2023-04-19 16:33:28 +0100

    backend: test: moved user role e2e test

--- backend/test/e2e/asUser.e2e-spec.ts
+++ backend/test/e2e/asUser.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../../src/app.module';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { MongooseModule } from '@nestjs/mongoose';
 
